54e81b6443bc113860942b979732eb549617aa96
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2023-05-29 22:19:09 -0400

    removeDuplicates solution

---
+++ com.testing.fictional-engine/src/main/java/org/example/RemoveDuplicatesSolution.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class RemoveDuplicatesSolution {
+    public static void main(String[] args) {
+
+
+    }
+
+    public static int removeDuplicates(int[] nums) {
+        int i = 0;
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[i] != nums[j]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i + 1;
+    }
+}
